a99e91f7caa7462eba5903ace31156046031d615
ahmedelmahd <ahmedsamir120495@gmail.com>
2026-01-17 21:36:41 +0200

    idk

--- backend/app/serializers/property_image_serializer.rb
+++ backend/app/serializers/property_image_serializer.rb
@@ -7,8 +7,7 @@ class PropertyImageSerializer < ActiveModel::Serializer
 
   def image_url
     if object.image.attached?
-      host = Rails.application.routes.default_url_options[:host] || 'localhost:3000'
-      rails_blob_url(object.image, host: host)
+      build_blob_url(object.image)
     else
       object.image_url
     end
@@ -16,11 +15,28 @@ class PropertyImageSerializer < ActiveModel::Serializer
 
   def blob_url
     if object.image.attached?
-      host = Rails.application.routes.default_url_options[:host] || 'localhost:3000'
-      rails_blob_url(object.image, host: host)
+      build_blob_url(object.image)
     else
       object.blob_url
     end
+  end
+
+  private
+
+  def build_blob_url(attachment)
+    # Build the URL manually to avoid action_mailer dependency in API-only apps
+    host = Rails.application.routes.default_url_options[:host] || 'localhost'
+    port = Rails.application.routes.default_url_options[:port]
+    protocol = Rails.application.routes.default_url_options[:protocol] || 'http'
+    
+    base_url = "#{protocol}://#{host}"
+    base_url += ":#{port}" if port
+    
+    path = rails_blob_path(attachment, only_path: true)
+    "#{base_url}#{path}"
+  rescue StandardError => e
+    Rails.logger.error("Failed to generate blob URL: #{e.message}")
+    nil
   end
 end
 
